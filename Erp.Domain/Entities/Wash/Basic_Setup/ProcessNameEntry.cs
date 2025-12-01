ead0a4ec158eecd472ad79c7dd3fc816958e3ee0
masumcsepust <masumcsepust@example.com>
2025-12-01 12:31:00 +0600

    Process Name Entry

---
+++ Erp.Domain/Entities/Wash/Basic_Setup/ProcessNameEntry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Erp.Domain.Entities.Wash.Basic_Setup
+{
+    public class ProcessNameEntry
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProcessId { get; set; }
+        public int UnitId { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(max)")]
+        public string ProcessName { get; set; }
+        public int Priority { get; set; }
+        public bool IsActive { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public int? DeletedBy { get; set; }
+        public DateTime? DeletedDate { get; set; }
+    }
+}
---
+++ Erp.WebApi/Controllers/Wash/Basic_Setup/ProcessNameEntryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Erp.WebApi.Controllers.Wash.Basic_Setup
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProcessNameEntryController : ControllerBase
+    {
+    }
+}
