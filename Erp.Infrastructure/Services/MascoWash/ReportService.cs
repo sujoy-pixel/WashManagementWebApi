c5c95732995c8f7768601615342b1970e06e7175
sujay <sujoy.cse.ku@gmail.com>
2026-02-05 11:05:55 +0600

    rpt add

--- Erp.Infrastructure/Services/MascoWash/ReportService.cs
+++ Erp.Infrastructure/Services/MascoWash/ReportService.cs
@@ -17,7 +17,36 @@ namespace Erp.Infrastructure.Services.MascoWash
             DataTable dt = new DataTable();
 
             // Replace with your actual connection string
-            string connectionString = "Data Source=192.168.50.77;Initial Catalog=MascoCommercialDB;User ID=sa;Password=1ndex@2023%24#new;";
+            string connectionString = "Data Source=192.168.50.77;Initial Catalog=MascoWashDB;User ID=sa;Password=1ndex@2023%24#new;";
+
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    // Execute the stored procedure and load the results into a DataReader
+                    using (var reader = await connection.ExecuteReaderAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure))
+                    {
+                        // Load the data into the DataTable
+                        dt.Load(reader);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Error executing stored procedure '{storedProcedure}': {ex.Message}");
+                throw; // Optionally rethrow or handle the exception
+            }
+
+            return dt;
+        }
+        public async Task<DataTable> GetDataByDataTableReadOnly(string storedProcedure, DynamicParameters parameters)
+        {
+            DataTable dt = new DataTable();
+
+            // Replace with your actual connection string
+            string connectionString = "Data Source=192.168.50.78;Initial Catalog=MascoWashDB;User ID=sa;Password=1ndex@2023%24#new;";
 
             try
             {
@@ -44,43 +73,12 @@ namespace Erp.Infrastructure.Services.MascoWash
         public string GetStoredProcedure(string ReportName)
         {
             string _storeProcedure = "";
-            if(ReportName== "Company And Master Lc Wise B2B Info")
+            if(ReportName== "Date Wise Batch Plan Report")
             {
                 _storeProcedure = "rpt_sp_CompanyAndMasterLcWiseB2BInfo";
                 
             }
-            else if(ReportName== "Master LC Report")
-            {
-                _storeProcedure = "sp_Get_DATA_Master_Report";
-            }else if(ReportName== "Goods Delivery Report")
-            {
-                _storeProcedure = "sp_Get_DATA_Goods_Service_Data";
-            }
-            else if (ReportName == "Proforma Invoice Report Local")
-            {
-                _storeProcedure = "sp_rpt_Proforma_Invoice_Local";
-            }
-            else if (ReportName == "Proforma Invoice Report Foreign")
-            {
-                _storeProcedure = "sp_rpt_Proforma_Invoice_Foreign";
-            }
-            else if (ReportName == "Company And Supplier Wise BTB LC Report")
-            {
-                _storeProcedure = "sp_get_company_supplier_wise_b2blc_report";
-            }
-            else if (ReportName == "Unit Wise Monthly Shipment Report")
-            {
-                _storeProcedure = "sp_rpt_month_wise_shipment_export";
-            }
-            else if (ReportName == "Day Wise Shipment Report")
-            {
-                _storeProcedure = "sp_rpt_day_wise_shipment_export";
-            }
-            else if (ReportName == "Monthly Shipment Report")
-            {
-                _storeProcedure = "sp_rpt_Monthly_Shipment_Report";
-            }
-
+            
             return _storeProcedure;
         }
        
