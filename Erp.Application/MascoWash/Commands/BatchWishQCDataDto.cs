843e9722e8d7e2a4487bb2ae1a6b215a494c43b2
Shah Newaz <shahnewaz385@gmail.com>
2026-03-29 16:37:35 +0600

    Main

--- Erp.Application/MascoWash/Commands/BatchWishQCDataDto.cs
+++ Erp.Application/MascoWash/Commands/BatchWishQCDataDto.cs
@@ -10,21 +10,36 @@ namespace Erp.Application.MascoWash.Commands
 
     {
         public int Id { get; set; }
-        public string BatchNo { get; set; }
-
-        public int BuyerId { get; set; }
+        public string UnitName { get; set; }
+        public int UnitId { get; set; }
         public string BuyerName { get; set; }
-
-        public int JobId { get; set; }
-        public string JobName { get; set; }
+        public int BuyerId { get; set; }
         public int StyleId { get; set; }
         public string StyleName { get; set; }
         public int OrderId { get; set; }
         public string OrderName { get; set; }
+        public int JobId { get; set; }
+        public string JobName { get; set; }
+
+        public string Type { get; set; }
+       
+        public string FabricationName { get; set; }
+      
+        public int FabricationId { get; set; }
         public int ColorId { get; set; }
         public string ColorName { get; set; }
+        public string DressPart { get; set; }
+        public int DressPartId { get; set; }
+
+        public string UOM { get; set; }
+        public int UomId { get; set; }
+
+        public DateTime PrepareDate { get; set; }
+        public string BatchNo { get; set; }
         public int MachineId { get; set; }
         public string MachineNo { get; set; }
+        public int ProcessId { get; set; }
+        public string ProcessName { get; set; }
         public int Priority { get; set; }
         public int SizeId { get; set; }
         public string Size { get; set; }
